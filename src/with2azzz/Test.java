9b0f088695a179a6231e0b7fedbb552c484e60f7
YeJee <2azzz@naver.com>
2017-11-18 16:44:55 -0800

    명애여신
    캬

---
+++ src/with2azzz/Test.java
@@ -0,0 +1,15 @@
+package with2azzz;
+
+public class Test {
+	public static void main(String[] args) {
+		String id = "testid";
+		String passwd = "1234";
+		if(id=="testid") {
+			if(passwd =="1234") {
+					System.out.println("로그인이 성공하였습니다.");
+					System.out.println("솔직히 명애여신 미만 잡");
+			}
+		}
+	}
+
+}
